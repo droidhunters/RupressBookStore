f778c3ce5d873338e6ff43b92979b61ff4a0ef97
droidhunters <info.droidhunters@gmail.com>
2016-06-25 11:21:41 +0700

    Intent to SignUp Finish

--- app/src/main/java/th/ac/ru/rupress/bookstore/rupressbooksstore/MainActivity.java
+++ app/src/main/java/th/ac/ru/rupress/bookstore/rupressbooksstore/MainActivity.java
@@ -1,7 +1,9 @@
 package th.ac.ru.rupress.bookstore.rupressbooksstore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }  //Main Method
+
+    public void ClickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SingUpActivity.class));
     }
-}
+} //Main Class
---
+++ app/src/main/java/th/ac/ru/rupress/bookstore/rupressbooksstore/SingUpActivity.java
@@ -0,0 +1,13 @@
+package th.ac.ru.rupress.bookstore.rupressbooksstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
