f9b870a2fd21b0e745b7b6a26b2c7f66e5c2353c
droidhunters <info.droidhunters@gmail.com>
2016-06-26 13:58:18 +0700

    Adapter

---
+++ app/src/main/java/th/ac/ru/rupress/bookstore/rupressbooksstore/BookAdapter.java
@@ -0,0 +1,43 @@
+package th.ac.ru.rupress.bookstore.rupressbooksstore;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by PLAN-NB on 6/26/2016.
+ */
+public class BookAdapter extends BaseAdapter {
+
+    //Explicit
+    private Context context;
+    private String[] nameStrings, priceStrings, coverStrings;
+
+    public BookAdapter(Context context, String[] nameStrings, String[] priceStrings, String[] coverStrings) {
+        this.context = context;
+        this.nameStrings = nameStrings;
+        this.priceStrings = priceStrings;
+        this.coverStrings = coverStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+} //Main Class
